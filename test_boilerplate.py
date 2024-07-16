c77e6aadd309df9c4e9828bc590e2821d6a8e27a
Jawad <jawad.olavian@gmail.com>
2024-07-16 14:31:49 +0100

    Wrote English input validation unit test
    Happy Birthday, Mr Savage!

--- test_boilerplate.py
+++ test_boilerplate.py
@@ -4,6 +4,29 @@ def myFunction(parameter): #replace with function imported from file
 
 print('_')
 print('Starting tests...')
-@pytest.mark.parametrize('data, expected', [(None,None),(None,None),(None,None)]) #replace with test data
+
+@pytest.mark.parametrize('data, expected', [
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None),
+    (None,None)
+    ]) #replace with test data
+
 def test_myFunction(data, expected):
     assert myFunction(data) == expected #replace with function imported from file
---
+++ test_englishValidation.py
@@ -0,0 +1,31 @@
+import pytest
+import textValidator
+
+print('_')
+print('Starting tests...')
+
+@pytest.mark.parametrize('data, expected', [
+    ('savage','SAVAGE'),
+    ('HAPPY','HAPPY'),
+    ('HAPPY BIRTHDAY MR SAVAGE','HAPPY BIRTHDAY MR SAVAGE'),
+    ('Happy Birthday, Mr Savage!','HAPPY BIRTHDAY, MR SAVAGE!'),
+    ('Happy Birthday, Mr Savage 🎉!','HAPPY BIRTHDAY, MR SAVAGE !'),
+    ('',False),
+    ('عيد ميلاد سعيد',False),
+    ('Nous avons fêté l\'anniversaire de M. Savage.','NOUS AVONS FTÉ L\'ANNIVERSAIRE DE M. SAVAGE.'),
+    ('Nous avons célébré l\'anniversaire de M. Savage.','NOUS AVONS CÉLÉBRÉ L\'ANNIVERSAIRE DE M. SAVAGE.'),
+    (' jsavage@saintolaves.net','JSAVAGE@SAINTOLAVES.NET'),
+    ('\#1 Mr Savage','1 MR SAVAGE'),
+    ('.... .- .--. .--. -.--','.... .- .--. .--. -.--'),
+    ('\"Happy Birthday, Mr Savage\"','\"HAPPY BIRTHDAY, MR SAVAGE\"'),
+    ('\'Happy Birthday, Mr Savage\'','\'HAPPY BIRTHDAY, MR SAVAGE\''),
+    ('+447596283679','+447596283679'),
+    ('   ',False),
+    ('Happy\nBirthday\nMr\nSavage','HAPPY BIRTHDAY MR SAVAGE'),
+    ('\n',False),
+    ('\#J\nSA\#V\n\#','J SAV '),
+    ('عيد ميلاد\n سعيد',False)
+    ]) 
+
+def test_validateEnglish(data, expected):
+    assert textValidator.validateEnglish(data) == expected
