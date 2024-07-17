49da9abe0a47f7166a684ffc0cc0a911d8271b95
Jawad <jawad.olavian@gmail.com>
2024-07-17 03:34:40 +0100

    Debugged Text class
    Happy Birthday, Mr Savage!

--- guiManager.py
+++ guiManager.py
@@ -5,18 +5,18 @@ class MorseMaster(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand = True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        container.pack(side = "top", fill = "both", expand = True)
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
 
         self.frames = {}
-        frame = Title(container, self)
-        self.frames[Title] = frame
-        frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame(Title)
+        frame = Text(container, text='HELLO!')
+        self.frames[Text] = frame
+        frame.grid(row = 0, column = 0, sticky = "nsew")
+        self.show_frame(Text)
 
         self.menu_bar = MenuBar(self)
-        self.config(menu=self.menu_bar.menubar)
+        self.config(menu = self.menu_bar.menubar)
 
     def show_frame(self, cont):
         frame = self.frames[cont]
@@ -26,23 +26,26 @@ class MenuBar:
     def __init__(self,app):
         self.menubar = tk.Menu(app)
 
-        file = tk.Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label='File', menu=file)
-        file.add_command(label='Exit', command=app.destroy)
+        file = tk.Menu(self.menubar, tearoff = 0)
+        self.menubar.add_cascade(label = 'File', menu = file)
+        file.add_command(label = 'Exit', command = app.destroy)
 
-        options = tk.Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label='Options', menu=options)
+        options = tk.Menu(self.menubar, tearoff = 0)
+        self.menubar.add_cascade(label='Options', menu = options)
 
-        help_ = tk.Menu(self.menubar, tearoff=0)
-        self.menubar.add_cascade(label='Help', menu=help_)
-        help_.add_command(label='MorseMaster Help', command=None)
-        help_.add_command(label='About MorseMaster', command=None)
+        help_ = tk.Menu(self.menubar, tearoff = 0)
+        self.menubar.add_cascade(label = 'Help', menu = help_)
+        help_.add_command(label = 'MorseMaster Help', command = None)
+        help_.add_command(label = 'About MorseMaster', command = None)
 
-class Title(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        label = tk.Label(self, text="MorseMaster", font=("Verdana", 36))
-        label.pack(pady=10,padx=10)
+class Text(tk.Frame):
+    def __init__(self, parent, text = 'MorseMaster', fontSize = 12, fontType = 'Verdana'):
+        self.fontSize = fontSize
+        self.fontType = fontType
+        self.text = text
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text = self.text, font = (self.fontType, self.fontSize))
+        label.pack(pady = 10, padx = 10)
 
 app = MorseMaster()
 app.iconbitmap('iconAssets/morseMasterIcon.ico')
