7398a9c577ec8e13094ead692059ad0d47cfc3ca
Jawad <jawad.olavian@gmail.com>
2024-07-18 14:52:46 +0100

    Completed text translator GUI layout
    Happy Birthday, Mr Savage!

--- guiManager.py
+++ guiManager.py
@@ -11,6 +11,7 @@ class MorseMaster(tk.Tk):
 
         self.tabBar = TabBar(self)
         self.tabBar.pack(side='top', fill='both', expand=True)
+        self.grid_columnconfigure(1, weight=1)
 
         self.menuBar = MenuBar(self)
         self.config(menu = self.menuBar.menubar)
@@ -56,25 +57,75 @@ class TabBar(ttk.Notebook):
         self.populateNetworkingTab()
 
     def populateTextTranslatorTab(self):
+        translationDirectionLabel = TextLabel(self.textTranslatorTab, text = 'English Plaintext -----> Morse Code Ciphertext', anchor = 'e', pady = (5,10))
+        translationDirectionLabel.grid(row = 0, column = 0)
+        translationDirectionLabel.tkraise()
+
+        translationDirectionButton = ButtonText(self.textTranslatorTab, text = 'Switch', command = None, pady = (3,10))
+        translationDirectionButton.grid(row = 0, column = 1, sticky = 'nw')
+        translationDirectionButton.tkraise()
+
         inputTextLabel = TextLabel(self.textTranslatorTab, text = 'Input English Plaintext:')
-        inputTextLabel.grid(row = 0, column = 0, sticky = 'nw')
+        inputTextLabel.grid(row = 1, column = 0, sticky = 'nw')
         inputTextLabel.tkraise()
 
+        uploadTextLabel = TextLabel(self.textTranslatorTab, text = 'Or upload a text file:', padx = (45,0))
+        uploadTextLabel.grid(row = 1, column = 2, sticky = 's')
+        uploadTextLabel.tkraise()
+
+        uploadButton = ButtonIcon(self.textTranslatorTab, filename = 'iconAssets/upload.png', command = None)
+        uploadButton.grid(row = 2, column = 2, sticky = 'n', padx = (45,0))
+        uploadButton.tkraise()
+
         inputTextArea = TextEntry(self.textTranslatorTab)
-        inputTextArea.grid(row = 1, column = 0, sticky = 'nw')
+        inputTextArea.grid(row = 2, column = 0, sticky = 'nw')
         inputTextArea.tkraise()
 
+        inputTextShortcutsFrame = tk.Frame(self.textTranslatorTab, width=30, height=85)
+        inputTextShortcutsFrame.pack_propagate(False)
+        inputTextShortcutsFrame.grid(row = 2, column = 1, sticky = 'nw')
+        inputTextShortcutsFrame.tkraise()
+
+        pasteButton = ButtonIcon(inputTextShortcutsFrame, filename = 'iconAssets/paste.png', command = None)
+        pasteButton.pack(side = 'top', fill = 'x', expand = True)
+        pasteButton.tkraise()
+
+        deleteButton = ButtonIcon(inputTextShortcutsFrame, filename = 'iconAssets/delete.png', command = None)
+        deleteButton.pack(side = 'bottom', fill = 'x', expand = True)
+        deleteButton.tkraise()
+
+        translateButton = ButtonText(self.textTranslatorTab, text = 'Translate', command = None, pady = (10,0))
+        translateButton.grid(row = 3, column = 1, sticky = 'nw')
+        translateButton.tkraise()
+
         outputTextLabel = TextLabel(self.textTranslatorTab, text = 'Output Morse Code Ciphertext:', pady = (20,0))
-        outputTextLabel.grid(row = 2, column = 0, sticky = 'nw')
+        outputTextLabel.grid(row = 3, column = 0, sticky = 'nw')
         outputTextLabel.tkraise()
 
+        downloadTextLabel = TextLabel(self.textTranslatorTab, text = 'Download as a text file:', padx = (45,0))
+        downloadTextLabel.grid(row = 3, column = 2, sticky = 's')
+        downloadTextLabel.tkraise()
+
+        downloadButton = ButtonIcon(self.textTranslatorTab, filename = 'iconAssets/download.png', command = None)
+        downloadButton.grid(row = 4, column = 2, sticky = 'n', padx = (45,0))
+        downloadButton.tkraise()
+
         outputTextArea = TextEntry(self.textTranslatorTab)
-        outputTextArea.grid(row = 3, column = 0, sticky = 'nw')
+        outputTextArea.grid(row = 4, column = 0, sticky = 'nw')
         outputTextArea.tkraise()
 
-        copyButton = ButtonIcon(self.textTranslatorTab, filename = 'iconAssets/copy.png')
-        copyButton.grid(row = 3, column = 1, sticky = 'nw')
+        outputTextShortcutsFrame = tk.Frame(self.textTranslatorTab, width=30, height=85)
+        outputTextShortcutsFrame.pack_propagate(False)
+        outputTextShortcutsFrame.grid(row = 4, column = 1, sticky = 'nw')
+        outputTextShortcutsFrame.tkraise()
+
+        copyButton = ButtonIcon(outputTextShortcutsFrame, filename = 'iconAssets/copy.png', command = None)
+        copyButton.pack(side = 'top', fill = 'x', expand = True)
         copyButton.tkraise()
+
+        lightButton = ButtonIcon(outputTextShortcutsFrame, filename = 'iconAssets/light.png', command = None)
+        lightButton.pack(side = 'bottom', fill = 'x', expand = True)
+        lightButton.tkraise()
 
     def populateSoundTranslatorTab(self):
         pass
@@ -122,8 +173,18 @@ class TextLabel(tk.Frame):
         label.pack(pady = self.pady, padx = self.padx)
 
 
-class ButtonText():
-    pass
+class ButtonText(tk.Frame):
+    def __init__(self, parent, command = None, text = 'Button', fontSize = 10, fontType = 'Verdana', pady = 0, padx = 0):
+        self.command = command
+        self.text = text
+        self.fontSize = fontSize
+        self.fontType = fontType
+        self.pady = pady
+        self.padx = padx
+        tk.Frame.__init__(self, parent)
+        button = tk.Button(self, text = self.text, font = (self.fontType, self.fontSize, 'bold'), command = self.command)
+        button.pack(pady = self.pady, padx = self.padx)
+
 
 class ButtonIcon(tk.Frame):
     def __init__(self, parent, command = None, filename = '', iconSize = (1,1), pady = 0, padx = 0):
@@ -159,5 +220,5 @@ class LightBox:
 app = MorseMaster()
 app.iconbitmap('iconAssets/morseMasterIcon.ico')
 app.title('MorseMaster')
-app.minsize(750,300)
+app.minsize(750,350)
 app.mainloop()
