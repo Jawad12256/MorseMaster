af7db3e03147540a3c8eeb643c089427d1cb8f56
Jawad <jawad.olavian@gmail.com>
2024-07-18 03:48:59 +0100

    ButtonIcon and widget classes cleanup
    Happy Birthday, Mr Savage!

--- guiManager.py
+++ guiManager.py
@@ -56,13 +56,25 @@ class TabBar(ttk.Notebook):
         self.populateNetworkingTab()
 
     def populateTextTranslatorTab(self):
-        inputTextLabel = TextLabel(self.textTranslatorTab, text = 'Input English plaintext:')
+        inputTextLabel = TextLabel(self.textTranslatorTab, text = 'Input English Plaintext:')
         inputTextLabel.grid(row = 0, column = 0, sticky = 'nw')
         inputTextLabel.tkraise()
 
         inputTextArea = TextEntry(self.textTranslatorTab)
-        inputTextArea.grid(row = 2, column = 0, sticky = 'nw')
+        inputTextArea.grid(row = 1, column = 0, sticky = 'nw')
         inputTextArea.tkraise()
+
+        outputTextLabel = TextLabel(self.textTranslatorTab, text = 'Output Morse Code Ciphertext:', pady = (20,0))
+        outputTextLabel.grid(row = 2, column = 0, sticky = 'nw')
+        outputTextLabel.tkraise()
+
+        outputTextArea = TextEntry(self.textTranslatorTab)
+        outputTextArea.grid(row = 3, column = 0, sticky = 'nw')
+        outputTextArea.tkraise()
+
+        copyButton = ButtonIcon(self.textTranslatorTab, filename = 'iconAssets/copy.png')
+        copyButton.grid(row = 3, column = 1, sticky = 'nw')
+        copyButton.tkraise()
 
     def populateSoundTranslatorTab(self):
         pass
@@ -98,29 +110,46 @@ class Networking:
 
 
 class TextLabel(tk.Frame):
-    def __init__(self, parent, text = 'MorseMaster', fontSize = 12, fontType = 'Verdana', anchor = 'w'):
+    def __init__(self, parent, text = 'MorseMaster', fontSize = 10, fontType = 'Verdana', anchor = 'w', pady = 5, padx = 10):
         self.fontSize = fontSize
         self.fontType = fontType
         self.text = text
         self.anchor = anchor
+        self.pady = pady
+        self.padx = padx
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text = self.text, font = (self.fontType, self.fontSize), anchor = self.anchor)
-        label.pack(pady = 5, padx = 10)
+        label.pack(pady = self.pady, padx = self.padx)
 
 
-class Button():
+class ButtonText():
     pass
+
+class ButtonIcon(tk.Frame):
+    def __init__(self, parent, command = None, filename = '', iconSize = (1,1), pady = 0, padx = 0):
+        self.command = command
+        self.filename = filename
+        self.iconSize = iconSize
+        self.pady = pady
+        self.padx = padx
+        icon = tk.PhotoImage(file = (self.filename)).subsample(self.iconSize[0])
+        tk.Frame.__init__(self, parent)
+        button = tk.Button(self, image = icon, command = self.command)
+        button.image = icon
+        button.pack(pady = self.pady, padx = self.padx)
 
 
 class TextEntry(tk.Frame):
-    def __init__(self, parent, fontSize = 12, fontType = 'Verdana', width = 40, height = 5):
+    def __init__(self, parent, fontSize = 10, fontType = 'Verdana', width = 40, height = 5, pady = 0, padx = 10):
         self.fontSize = fontSize
         self.fontType = fontType
         self.width = width
         self.height = height
+        self.pady = pady
+        self.padx = padx
         tk.Frame.__init__(self, parent)
         textBox = ScrolledText(self, font = (self.fontType, self.fontSize), width = self.width, height = self.height)
-        textBox.pack(pady = 0, padx = 10, fill='y')
+        textBox.pack(pady = self.pady, padx = self.padx, fill='y')
 
 
 class LightBox:
