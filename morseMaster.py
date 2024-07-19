d6d53f2b6105d5ab64b501eb218e648bf283205c
Jawad <jawad.olavian@gmail.com>
2024-07-19 01:11:42 +0100

    Label switching button functionality
    Happy Birthday, Mr Savage!

--- morseMaster.py
+++ morseMaster.py
@@ -11,4 +11,17 @@ for widgetObject in textTranslatorWidgetObjects:
     if hasattr(widgetObject, 'Name'):
         textTranslatorObject[widgetObject.Name] = widgetObject
 
+def switch():
+    mainLabel, inputLabel, outputLabel = textTranslatorObject['translationDirectionLabel'], textTranslatorObject['inputTextLabel'], textTranslatorObject['outputTextLabel']
+    if mainLabel.getText() == 'English Plaintext -----> Morse Code Ciphertext':
+        mainLabel.setText('Morse Code Ciphertext -----> English Plaintext')
+        inputLabel.setText('Input Morse Code Ciphertext')
+        outputLabel.setText('Output English Plaintext')
+    else:
+        mainLabel.setText('English Plaintext -----> Morse Code Ciphertext')
+        inputLabel.setText('Input English Plaintext:')
+        outputLabel.setText('Output Morse Code Ciphertext:')
+
+textTranslatorObject['translationDirectionButton'].button.configure(command = switch)
+
 app.mainloop()
