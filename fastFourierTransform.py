62d9b4fa9d0849e912a96bc5649fa20b8a4cc08c
Jawad <jawad.olavian@gmail.com>
2024-07-27 16:08:46 +0100

    Implemented DFT and FFT algorithms
    Happy Birthday, Mr Savage!

--- fastFourierTransform.py
+++ fastFourierTransform.py
@@ -1,15 +1,37 @@
 '''FAST FOURIER TRANSFORM MODULE'''
 import numpy as np
 
-def DFT(x):
-    N = len(x)
-    n = np.arange(N)
-    k = n.reshape((N, 1))
+def DFT(x, sampleRate):
+    N = len(x) #number of samples
+    n = np.arange(N) #list of integers up to N non-inclusive
+    k = n.reshape((N, 1)) #use views to reduce memory use
     e = np.exp(-2j * np.pi * k * n / N)
-    X = np.dot(e, x)
+    X = np.dot(e, x) #multiply across by list of sine values
+    T = N/sampleRate
+    freq = n/T #calculate frequencies list
+    return (X, freq)
+
+def FFTrecurse(x):
+    N = len(x)
+
+    if np.log2(N) % 1 > 0:
+        x = np.pad(x, (0, 2**int(np.ceil(np.log2(N))) - N))
+        N = len(x)
     
+    if N == 1:
+        return x
+    else:
+        even = FFTrecurse(x[::2])
+        odd = FFTrecurse(x[1::2])
+        factor = np.exp(-2j*np.pi*np.arange(N)/ N)
+        
+        X = np.concatenate([even+factor[:int(N/2)]*odd, even+factor[int(N/2):]*odd])
+        return X
+    
+def FFT(x, sampleRate):
+    X = FFTrecurse(x)
     N = len(X)
     n = np.arange(N)
-    T = N/100
-    freq = n/T 
-    return (X, freq)
+    T = N/sampleRate
+    freq = n/T
+    return (X,freq)
--- soundTranslator.py
+++ soundTranslator.py
@@ -4,30 +4,14 @@ import wavio as wv
 from matplotlib import pyplot as plt
 import fastFourierTransform as fft
 
-def sineWave(frequency, sampleRate, duration):
-    t = np.linspace(0, duration, sampleRate * duration, endpoint=False)
-    f = np.sin((2 * np.pi) * t * frequency)
-    return f
-
-tone1 = sineWave(1, 2000, 5)
-tone2 = sineWave(70, 2000, 5)
-noisyTone = tone1 + tone2 * 0.3
-scaledTone = np.int16((noisyTone / noisyTone.max()) * 32767)
-
-
-def gen_sig(sampleRate):
+def sineWave(frequency, ampScale, sampleRate):
     t = np.arange(0,1,1.0/sampleRate)
-    freq = 1.
-    x = 3*np.sin(2*np.pi*freq*t)
-    return x
+    sine = ampScale*np.sin((2 * np.pi) * t * frequency)
+    return sine
 
-samplingRate = 2000
-X = fft.DFT(scaledTone)
+myWave128 = sineWave(1, 3, 128) + sineWave(4, 1, 128) + sineWave(7, 0.5, 128)
 
-# t = np.arange(0,1,1.0/samplingRate)
-# w = 3*np.sin(2*np.pi*1*t) + 1*np.sin(2*np.pi*15*t) + 0.5*np.sin(2*np.pi*45*t)
+Y = fft.FFT(myWave128, 128)
 
-# X = fft.DFT(w)
-
-plt.stem(X[1],abs(X[0]))
+plt.stem(Y[1],abs(Y[0]))
 plt.show()
